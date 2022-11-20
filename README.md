dd587d9ed8c755e1640f59a35fb267bf2554ade1
unknown <cleide.edificacoes@hotmail.com>
2022-11-20 16:00:26 -0300

    Labebank

--- README.md
+++ README.md
@@ -1,2 +1,30 @@
 # Projeto em grupo - LabeBank
-## Atenção! Personalize seu readme!
+
+<!-- SISTEMA BANCÁRIO
+
+Estruturas de Dados
+Contas:
+  •	Nome
+  •	CPF
+  •	Data de nascimento (idade>18)
+  •	Saldo(começa zerado)
+  • Extrato(array de transações)
+	  •	Valor
+      •	Data
+      •	Descrição
+Funcionalidade
+Criar conta
+  •	Informar nome, CPF, e data de nascimento
+Pegar saldo
+  •	Passando nome e CPF
+Adicionar saldo
+  •	Passando nome, CPF  e valor
+Pagar conta
+  •	Passando um valor, uma descrição e uma data de pagamento.
+  •	Caso ele não informe a date,  considerar pagamento no mesmo dia.
+  •	Não pode agendar um pagamento para um dia que já passou.
+  •	Não pode haver pagamento para saldos insuficientes
+Transferência Interna
+  •	Informar o nome, o  CPF, o nome do destinatário, o CPF do destinatário e o valor.
+  •	Não podem ser agendadas -->
+
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,59 @@
-import express from "express"
-
+import express, {Request,Response} from 'express'
 import cors from 'cors'
+import { UsuarioDaConta } from './types'
+import { users } from './data'
 
 const app = express()
 
 app.use(express.json())
-
 app.use(cors())
 
+app.post("/users/addUser", (req:Request, res:Response)=>{
+let errorCode = 400
 
+try{
+    const {nome, cpf, aniversario} = req.body
+if(!nome || !cpf || !aniversario){
+errorCode = 422
+throw new Error("Falta passar paramentros, nome CPF, data de Nascimento");
+}
+
+let idadeUsuario = aniversario.split('/')
+let dataAtual = new Date()
+let stringData = idadeUsuario[1] + '-' + idadeUsuario[0] + '-' + idadeUsuario[2];
+let idade = new Date(stringData);
+let daIdade = idade.setFullYear(idade.getFullYear()+18)
+console.log('Data formatada 2:' + idade)
+console.log('Data de hoje:' + dataAtual)
+
+  if(idade > dataAtual){
+    errorCode = 401
+    throw new Error("Menor de 18 anos. Usuário não autorizado!");
+  }
+
+  const novoUsuario : UsuarioDaConta ={
+    nome : nome,
+    cpf : cpf,
+    aniversario : aniversario,
+    // balancoDaConta : balancoDaConta,
+    extrato : []
+  }
+
+  users.push(novoUsuario)
+
+  res.status(201).send(users)
+
+} catch (error:any){
+    res.status(errorCode).send(error.message)
+}
+
+})
+
+app.get('/users',(req:Request,res:Response)=>{
+  res.send(users)
+})
 
 app.listen(3003, () => {
     console.log("Server is running in http://localhost:3003");
 });
+
